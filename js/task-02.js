d8fd9d07a519a03d6fcac940b5ac14683277072a
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-02 00:05:56 +0200

    adedd dicision task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add("item");
+  console.log(itemEl);
+  listEl.appendChild(itemEl);
+});
