0d2c6bb758307ac117445b1b35bf83c6a2cf28b4
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-03 23:31:15 +0200

    add decision task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,43 +1,8 @@
-//Порахує і виведе в консоль кількість категорій
-// в ul#categories, тобто елементів li.item.
-//Для кожного элемента li.item у списку ul#categories,
-//знайде і виведе в консоль текст заголовку елемента (тегу <h2>)
-// і кількість елементів в категорії (усіх <li>, вкладених в нього).
+const itemEl = document.querySelectorAll(".item");
 
-//Number of categories: 3
-
-//Category: Animals
-//Elements: 4
-
-//Category: Products
-//Elements: 3
-
-//Category: Technologies
-//Elements: 5
-
-const listEl = document.querySelector("#categories");
-const itemEl = listEl.children;
-const titleEl = itemEl[0].firstElementChild;
-
-const titleItemEl = titleEl.nextElementSibling;
-const allLiEl = titleItemEl.children;
-//
-console.log(listEl);
-console.log(itemEl);
-console.log(titleEl);
-
-console.log(titleItemEl);
-console.log(allLiEl);
 console.log(`Number of categories: ${itemEl.length}`);
-console.log(`Category: ${titleEl}`);
-console.log(`Elements: ${allLiEl.length}`);
-console.log(`Category: ${titleEl}`);
-console.log(`Elements: ${titleItemEl}`);
-console.log(`Category: ${titleEl}`);
-console.log(`Elements: ${titleItemEl}`);
 
-// firstElementChild;
-// lastElementChild;
-// children;
-// childNodes;
-// parentNode;
+itemEl.forEach((element) => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
