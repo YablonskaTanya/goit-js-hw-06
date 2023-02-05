74f5be58b0ea5d398e606cfee93499ada46ed555
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-05 20:20:50 +0200

    added decision task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorEl = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", () => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorEl.textContent = color;
+});
