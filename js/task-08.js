83fb5451f7809e1dd68e0aa1a52d0245ba679835
YablonskaTanya <111698588+YablonskaTanya@users.noreply.github.com>
2023-02-13 13:53:16 +0200

    added tix task 8

--- js/task-08.js
+++ js/task-08.js
@@ -13,12 +13,25 @@ function onFormSubmit(event) {
     password,
   };
 
-  if (mail === "" || password === "") {
-    alert("всі поля повиннібути заповнені");
-  } else {
-    console.log(formData);
-  }
-  console.log(`${formElements.email.name} - ${mail}`);
-  console.log(`${formElements.password.name} - ${password}`);
+  !mail || !password
+    ? alert("всі поля повиннібути заповнені")
+    : console.log(formData);
+
+  console.log(`${formElements.email.name}: ${mail}`);
+  console.log(`${formElements.password.name}: ${password}`);
   event.currentTarget.reset();
 }
+
+// if (mail === "" || password === "") {
+//   alert("всі поля повиннібути заповнені");
+// } else {
+//   console.log(formData);
+// }
+//
+//
+// if (!mail || !password) {
+//   alert("всі поля повиннібути заповнені");
+// } else {
+//   console.log(formData);
+// }
+//
